202d45b86357fd01d797f33888512ba48aa56042
Francesco Brischetto <francesco_brischetto@hotmail.it>
2023-05-13 19:51:46 +0200

    Added quit button and created build

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -59,6 +59,16 @@ public class MainMenuController : MonoBehaviour
         RandomPercentage.y = max / 100;
     }
 
+    public void QuitGame()
+    {
+        #if UNITY_STANDALONE
+                Application.Quit();
+        #endif
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+
     public void StartGame()
     {
         PlayerPrefs.SetInt("playerNumber", playerNumber);
